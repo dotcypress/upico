655776ae84e7939bfa2141a4c8ec2aceb42329d5
Vitaly Domnikov <dotcypress@gmail.com>
2023-10-28 14:21:22 -0700

    installer script

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-echo "Installing upico"
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  echo "uPico installer is not running as root. Try using sudo."
+  exit 2
+fi
+
+cp upico /usr/local/bin/
+cp upico.service /etc/systemd/system/
+systemctl enable upico
+systemctl start upico
+echo "uPico installed."
