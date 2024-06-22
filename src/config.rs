47fea82b302445008ee340e396ce6e0147323b65
Vitaly Domnikov <dotcypress@gmail.com>
2024-06-22 13:41:36 -0700

    bookworm linux support

--- src/config.rs
+++ src/config.rs
@@ -27,6 +27,20 @@ pub mod platform {
     pub const PIN_USB_OCP: usize = 20;
 }
 
+#[cfg(feature = "cm4-bookworm")]
+pub mod platform {
+    pub const AUX_SWITCH: bool = true;
+    pub const OCP_REPORTING: bool = true;
+    pub const PIN_PICO_BOOT: usize = 2;
+    pub const PIN_VDD_EN: usize = 25;
+    pub const PIN_USB_EN: usize = 29;
+    pub const PIN_PICO_RUN: usize = 22;
+    pub const PIN_AUX_EN: usize = 27;
+    pub const PIN_AUX_OCP: usize = 11;
+    pub const PIN_VDD_OCP: usize = 6;
+    pub const PIN_USB_OCP: usize = 28;
+}
+
 #[cfg(feature = "a06")]
 pub mod platform {
     pub const OCP_REPORTING: bool = false;
